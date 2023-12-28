598e4a67cff0c01631fe38abe7f82261a30d01db
Giacomo Salici <giambo2000@gmail.com>
2023-12-28 22:38:18 +0100

    closest_list final

--- global_planner/closest_list.py
+++ global_planner/closest_list.py
@@ -1,4 +1,5 @@
 import numpy as np
+import json, sys
 
 def find_nearest_neighbor(point, points_array):
     distances = np.linalg.norm(points_array - point, axis=1)
@@ -6,10 +7,25 @@ def find_nearest_neighbor(point, points_array):
     nearest_neighbor = points_array[nearest_index]
     return nearest_neighbor
 
-import json, sys
+
+
 d = json.load(open(sys.argv[1]))
+
+
 b1 = np.array(d["yellow"])
-b2 = d["blue"]
+b2 = np.array(d["blue"])
+
+
 nearest_neighbors = np.array([[point, find_nearest_neighbor(point, b2)] for point in b1])
 
+
+
 print("Nearest neighbor:", nearest_neighbors)
+
+centers = np.sum(nearest_neighbors, axis=1) / 2
+
+# Calculate the distances
+distances_point1 = np.linalg.norm(centers - nearest_neighbors[:, 0, :], axis=1).reshape(-1, 1)
+distances_point2 = np.linalg.norm(centers - nearest_neighbors[:, 1, :], axis=1).reshape(-1, 1)
+exp = np.concatenate((centers, distances_point1, distances_point2), axis=1)
+print("exp", exp)
