6d74370cbedc2460974a97356774a0aeb9a04e23
Giacomo Salici <giambo2000@gmail.com>
2024-01-02 00:09:33 +0100

    a bit better but still not perfectly working

--- global_planner/global_track.py
+++ global_planner/global_track.py
@@ -1,8 +1,7 @@
 import numpy as np
-from scipy.interpolate import CubicSpline
+from scipy.interpolate import splprep, splev
 from scipy.optimize import minimize_scalar 
-
-import numpy as np
+import matplotlib.pyplot as plt
 from enum import Enum
 
 class Track():
@@ -17,13 +16,17 @@ class Track():
         return self.lines.YELLOW in self.__points and self.lines.BLUE in self.__points
 
     def __create_spline(self, points):
-        return CubicSpline(np.arange(len(points)), points)
+        tck, u = splprep(points.T, per=True)
+        return tck
 
     def __distance(self, midline_point, boundary_spline):
-        # Use a numerical optimization method to find the minimum distance along the spline
-        t_closest = minimize_scalar(lambda t: np.linalg.norm(midline_point-boundary_spline(t)), bounds=(0, len(boundary_spline.c) - 1), method='bounded').x
-        return np.linalg.norm(midline_point- boundary_spline(t_closest))
+        distance_function = lambda t: np.linalg.norm(midline_point - np.array(splev(t, boundary_spline), dtype=np.float))
 
+        # Use minimize_scalar to find the minimum distance along the spline
+        result = minimize_scalar(distance_function, bounds=(0, 1), method='bounded')
+
+        return result.fun
+    
     def __find_distances(self, midline, boundary):
         boundary_spline = self.__create_spline(boundary)
         distances_midline = [self.__distance(point, boundary_spline) for point in midline]
@@ -31,8 +34,9 @@ class Track():
         return np.array(distances_midline)
     
     def create_reftrack(self):
+        print(self.__points)
         w_l = self.__find_distances(self.__points[self.lines.TRACK], self.__points[self.lines.BLUE]).reshape(-1,1)
-        w_r = self.__find_distances(self.__points[self.lines.TRACK], self.__points[self.lines.YELLOW]).reshape(-1,1)
+        w_r = self.__find_distances(self.__points[self.lines.TRACK], self.__points[self.lines.YELLOW  ]).reshape(-1,1)
 
         self.__reftrack = np.concatenate((self.__points[self.lines.TRACK], w_r, w_l), axis=-1)
 
@@ -41,23 +45,30 @@ class Track():
     def is_reftrack_created(self):
         return self.__reftrack != None
 
-# for testing
+
+
+
+# just for testing
 
 def main():
 
+    num_points = 10
+    theta = np.linspace(0, 2*np.pi, num_points, endpoint=False)
 
-    # Example usage
-    midline = np.array(  [[1, 2], [3, 4], [5, 6]])
-    boundary = np.array( [[0, 0], [2, 2], [4, 4], [5,5]])
-    boundary2 = np.array([[3, 0], [6, 3], [9, 6]])
+    l = {}
 
+    for radius in [8,10,9]:
+            x = radius * np.cos(theta)
+            y = radius * np.sin(theta)
+            l[radius] = np.squeeze(np.dstack((x,y)), axis=0)
 
     
     t = Track()
 
-    t.add_line(points=midline, line=t.lines.TRACK)
-    t.add_line(points=boundary2, line=t.lines.BLUE)
-    t.add_line(points=boundary, line=t.lines.YELLOW)
+
+    t.add_line(points=l[9], line=t.lines.TRACK)
+    t.add_line(points=l[10], line=t.lines.BLUE)
+    t.add_line(points=l[8], line=t.lines.YELLOW)
 
     if t.has_boundaries() and not t.is_reftrack_created():
         t.create_reftrack()
