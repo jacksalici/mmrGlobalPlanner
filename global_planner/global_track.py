c06e269f6aa04cb429bc9dc5a299a18474060524
Giacomo Salici <giambo2000@gmail.com>
2024-01-04 22:18:39 +0100

    better distance

--- global_planner/global_track.py
+++ global_planner/global_track.py
@@ -1,6 +1,4 @@
 import numpy as np
-from scipy.interpolate import splprep, splev
-from scipy.optimize import minimize_scalar 
 import matplotlib.pyplot as plt
 from enum import Enum
 
@@ -9,32 +7,31 @@ class Track():
     __points = {}
     __reftrack = None
 
+    def __init__(self, debug = False) -> None:
+        if debug:
+            self.debug = True
+
     def add_line(self, line: lines, points: list):
         self.__points[line] = np.array(points, dtype=np.float)
 
     def has_boundaries(self) -> bool:
         return self.lines.YELLOW in self.__points and self.lines.BLUE in self.__points
-
-    def __create_spline(self, points):
-        tck, u = splprep(points.T, per=True)
-        return tck
-
-    def __distance(self, midline_point, boundary_spline):
-        distance_function = lambda t: np.linalg.norm(midline_point - np.array(splev(t, boundary_spline), dtype=np.float))
-
-        # Use minimize_scalar to find the minimum distance along the spline
-        result = minimize_scalar(distance_function, bounds=(0, 1), method='bounded')
-
-        return result.fun
     
-    def __find_distances(self, midline, boundary):
-        boundary_spline = self.__create_spline(boundary)
-        distances_midline = [self.__distance(point, boundary_spline) for point in midline]
+    def __distance(self, point, segmentsA, segmentsB):
+        cross_product = np.cross(segmentsA - point, point - segmentsB)
+        norm_b_minus_a = np.linalg.norm(segmentsB - segmentsA, axis=1)
+        distance = np.min(np.abs(cross_product) / norm_b_minus_a)
+        if self.debug:
+            print(f"Min distance for {point}: {distance}")
+        return distance
+    
+    
+    def __find_distances(self, midline_points, boundaries):
+        return np.array([self.__distance(point, boundaries[:-1, :], boundaries[1:, :]) for point in midline_points])
+        
 
-        return np.array(distances_midline)
     
     def create_reftrack(self):
-        print(self.__points)
         w_l = self.__find_distances(self.__points[self.lines.TRACK], self.__points[self.lines.BLUE]).reshape(-1,1)
         w_r = self.__find_distances(self.__points[self.lines.TRACK], self.__points[self.lines.YELLOW  ]).reshape(-1,1)
 
@@ -63,7 +60,7 @@ def main():
             l[radius] = np.squeeze(np.dstack((x,y)), axis=0)
 
     
-    t = Track()
+    t = Track(debug=True)
 
 
     t.add_line(points=l[9], line=t.lines.TRACK)
